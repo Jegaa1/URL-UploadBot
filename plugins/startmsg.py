07241b80a3412aecb8e95b1680ed44cab0565867
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-04-13 20:36:56 +0530

    Update startmsg.py

--- plugins/startmsg.py
+++ plugins/startmsg.py
@@ -20,12 +20,12 @@ Or use /cancel to terminate incomplete processes."""
 You can now send a new URL."""
     NO_PROCESS_FOUND = """🤷‍♂️ No pending uploads were found. You can upload files by sending a link now!"""
     FORMAT_SELECTION = "👇𝗦𝗲𝗹𝗲𝗰𝘁 𝗔𝗻𝗱 𝗖𝗵𝗼𝘀𝗲 𝗬𝗼𝘂𝗿 𝗙𝗼𝗿𝗺𝗮𝘁👇"
-    SET_CUSTOM_USERNAME_PASSWORD = """\n👮‍♂ Powered By :</b> @HxBots"""
+    SET_CUSTOM_USERNAME_PASSWORD = """\n👮‍♂ Powered By :</b> @JAsuranbots"""
     DOWNLOAD_START = "📥 DOWNLOADING..."
     UPLOAD_START = "📤 UPLOADING..."
     RCHD_TG_API_LIMIT = "<b>Downloaded in:</b> {} seconds.\n<b>Detected file size:</b> {}.\n\nSorry. But, I cannot upload files greater than 2GB due to Telegram API limitations 😕."
     #AFTER_SUCCESSFUL_UPLOAD_MSG = "𝘛𝘏𝘈𝘕𝘒𝘚 𝘍𝘖𝘙 𝘜𝘚𝘐𝘕𝘎 𝘔𝘌 🥰"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "𝘛𝘏𝘈𝘕𝘒𝘚 𝘍𝘖𝘙 𝘜𝘚𝘐𝘕𝘎 𝘔𝘌 🥰\n\n@HxBots | @HxSupport"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "𝘛𝘏𝘈𝘕𝘒𝘚 𝘍𝘖𝘙 𝘜𝘚𝘐𝘕𝘎 𝘔𝘌"
     SAVED_CUSTOM_THUMB_NAIL = "Save Your Thumbnail ✅."
     DEL_ETED_CUSTOM_THUMB_NAIL = " Delete Your Thumbnail ✅."
     CUSTOM_CAPTION_UL_FILE = "{}"
@@ -49,7 +49,7 @@ You can now send a new URL."""
 """
     ABOUT_TEXT = """<b>🔘 My Name :</b> URL-UploadBot V3 🚀
 
-<b>🔘 Channel :</b> <a href="https://t.me/HxBots">HxBots</a>
+<b>🔘 Channel :</b> <a href="https://t.me/JAsuranbots">JAsuranBots</a>
 
 <b>🔘 Source :</b> <a href="https://github.com/oVo-HxBots/URL-UploadBot">Soon..</a>
 
